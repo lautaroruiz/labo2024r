b2332280ed6be3dbc8eee7a21b123d8b6588ce60
LautaroRuiz <lautaroruizf@gmail.com>
2024-08-01 22:33:47 +0000

    fix exp name pao

--- src/workflows/719_run_epic_019.r
+++ src/workflows/719_run_epic_019.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/719_workflow_epic_f202109_004.r"
+PARAM <- "src/workflows/719_workflow_epic_f202109_019.r"
 
 envg <- env()
 
