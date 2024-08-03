9e249f8440beb1cd80db125e67835b53a0816bff
LautaroRuiz <lautaroruizf@gmail.com>
2024-08-03 15:19:36 +0000

    me olvide de excluir completamente la base

--- src/workflows/719_workflow_epic_f202109_023.r
+++ src/workflows/719_workflow_epic_f202109_023.r
@@ -247,8 +247,8 @@ CN_canaritos_asesinos_base <- function( pinputexps, ratio, desvio)
   # Parametros de un LightGBM que se genera para estimar la column importance
   param_local$train$clase01_valor1 <- c( "BAJA+2", "BAJA+1")
   param_local$train$positivos <- c( "BAJA+2")
-  param_local$train$training <- c( 202101, 202102, 202103)
-  param_local$train$validation <- c( 202105 )
+  param_local$train$training <- c( 202019, 202101, 202102)
+  param_local$train$validation <- c( 202104 )
   param_local$train$undersampling <- 0.1
   param_local$train$gan1 <- 117000
   param_local$train$gan0 <-  -3000
